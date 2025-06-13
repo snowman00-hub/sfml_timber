a728063ebf3d144b6803d522cf643a74e4287ced
윤준모 <ojm01088021296@gmail.com>
2025-06-13 15:37:56 +0900

    배경 추가

--- main.cpp
+++ main.cpp
@@ -2,23 +2,30 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Timber!");
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "RemakeTimber");
 
-    sf::Texture textureBackground;
-    textureBackground.loadFromFile("graphics/background.png");
+    // 리소스
+    sf::Texture background;
+    background.loadFromFile("graphics/background.png");
 
+    // 리소스 실행객체
     sf::Sprite spriteBackground;
-    spriteBackground.setTexture(textureBackground);
+    spriteBackground.setTexture(background);
 
     while (window.isOpen())
     {
         sf::Event event;
+        // 이벤트 반복
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
+        // 업데이트
+
+
+        // 그리기
         window.clear();
 
         window.draw(spriteBackground);
